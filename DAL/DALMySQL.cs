28332d304898a5a03b3acd78a0c24333687ed410
devstackpro <devstackpro@gmail.com>
2022-03-02 13:38:47 -0300

    correções na estrutura do projeto

---
+++ DAL/DALMySQL.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace DAL
+{
+    public class DALMySQL
+    {
+        //Criar Conexao Banco de Dados
+        private MySqlConnection CriarConexao()
+        {
+            String connString = null;
+            StreamReader sr = File.OpenText("stringConexaoMySql.txt");
+            string input = null;
+            while ((input = sr.ReadLine()) != null)
+            {
+                connString = input;
+            }
+            return new MySqlConnection(connString);
+            //return new SqlConnection(Settings.Default.stringConexao);
+        }
+
+        //Parametros que vao para o banco
+        private MySqlParameterCollection mySqlParameterCollection = new MySqlCommand().Parameters;
+
+        public void LimparParametros()
+        {
+            mySqlParameterCollection.Clear();
+        }
+
+        public void AdicionaParametros(string nomeParametro, object valorParametro)
+        {
+            mySqlParameterCollection.Add(new MySqlParameter(nomeParametro, valorParametro));
+        }
+
+        //Persistencia - inserir/alterar/excluir
+        public object ExecutarManipulacao(CommandType commandType, string nomeProcedore)
+        {   //Criar conexao
+            MySqlConnection mySqlConnection = CriarConexao();
+            try
+            {
+                //Criar conexao
+                //MySqlConnection mySqlConnection = CriarConexao();
+                //Abrir Conexao
+                mySqlConnection.Open();
+                //Criar comando que leva a informaçao para o banco
+                MySqlCommand mySqlCommand = mySqlConnection.CreateCommand();
+                //Alimentando o comando
+                //sqlCommand.CommandType = CommandType.StoredProcedure;
+                //sqlCommand.CommandType = CommandType.Text;
+                mySqlCommand.CommandType = commandType;
+                mySqlCommand.CommandText = nomeProcedore;
+                mySqlCommand.CommandTimeout = 20200; //20 segundos
+                //Adiciona os parametros no comando
+                foreach (MySqlParameter mySqlParameter in mySqlParameterCollection)
+                {
+                    mySqlCommand.Parameters.Add(new MySqlParameter(mySqlParameter.ParameterName, mySqlParameter.Value));
+                }
+
+                //Executar Comando
+                return mySqlCommand.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                //fecha Conexao
+                mySqlConnection.Close();
+
+            }
+        }
+
+        //Consultar registros do banco dados
+        public DataTable ExecutarConsulta(CommandType commandType, string nomeProcedore)
+        {
+            try
+            {
+                //Criar conexao
+                MySqlConnection mySqlConnection = CriarConexao();
+                //Abrir Conexao
+                mySqlConnection.Open();
+                //Criar comando que leva a informaçao para o banco
+                MySqlCommand mySqlCommand = mySqlConnection.CreateCommand();
+                //Alimentando o comando
+                //sqlCommand.CommandType = CommandType.StoredProcedure;
+                //sqlCommand.CommandType = CommandType.Text;
+                mySqlCommand.CommandType = commandType;
+                mySqlCommand.CommandText = nomeProcedore;
+                mySqlCommand.CommandTimeout = 20200; //20 segundos
+                //Adiciona os parametros no comando
+                foreach (MySqlParameter mySqlParameter in mySqlParameterCollection)
+                {
+                    mySqlCommand.Parameters.Add(new MySqlParameter(mySqlParameter.ParameterName, mySqlParameter.Value));
+                }
+                //Criar Adaptador
+                MySqlDataAdapter mySqlDataAdapter = new MySqlDataAdapter(mySqlCommand);
+                //Tabela de dados Vazia
+                DataTable dataTable = new DataTable();
+                //comando vai ao banco e busca os dados
+                mySqlDataAdapter.Fill(dataTable);
+                //fecha conexao
+                //mySqlConnection.Close();
+                return dataTable;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+        }
+    }
+}
---
+++ DAO/DAOShiftReport.cs
@@ -0,0 +1,38 @@
+﻿namespace DAO
+{
+    public class DAOShiftReport
+    {
+        public string Sortkey { get; set; }
+        public string Date { get; set; }
+        public string Loom { get; set; }
+        public string Style { get; set; }
+        public string RPM { get; set; }
+        public string EfficPercent { get; set; }
+        public string RunMinute { get; set; }
+        public string StopMinute { get; set; }
+        public string ProductPick { get; set; }
+        public string WarpCount { get; set; }
+        public string WarpMinute { get; set; }
+        public string WarpRatePH { get; set; }
+        public string WarpRatePDAY { get; set; }
+        public string WarpRatePP { get; set; }
+        public string Weft_Count { get; set; }
+        public string WeftMinute { get; set; }
+        public string WeftRatePH { get; set; }
+        public string WeftRatePDAY { get; set; }
+        public string WeftRatePP { get; set; }
+        public string UnselectCount { get; set; }
+        public string UnselectMinute { get; set; }
+        public string Unselect2Count { get; set; }
+        public string UnselectRatePH { get; set; }
+        public string UnselectRatePDAY { get; set; }
+        public string UnselectRatePP { get; set; }
+        public string TotalCount { get; set; }
+        public string TotalMinute { get; set; }
+        public string Total2Count { get; set; }
+        public string TotalRatePH { get; set; }
+        public string TotalRatePDAY { get; set; }
+        public string TotalRatePP { get; set; }
+
+    }
+}
