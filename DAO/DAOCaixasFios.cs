321e89c81d19f06470e7b0496f1d3e30af68c6c0
Tiago de Abreu <devtiagoabreu@gmail.com>
2023-01-20 11:12:57 -0300

    Objetos de Caixas de Fios

---
+++ DAO/DAOCaixasFios.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    public class DAOCaixasFios
+    {
+        public string Emp { get; set; }
+        public string Empresa { get; set; }
+        public string Dep { get; set; }
+        public string Deposito { get; set; }
+        public string Tran { get; set; }
+        public string Transacao { get; set; }
+        public string Produto { get; set; }
+        public string Nivel { get; set; }
+        public string Grupo { get; set; }
+        public string Sub { get; set; }
+        public string Item { get; set; }
+        public string Um { get; set; }
+        public string Narrativa { get; set; }
+        public string Tpg { get; set; }
+        public string TipoGlobal { get; set; }
+        public string Lote { get; set; }
+        public string LoteProduto { get; set; }
+        public decimal Quantidade { get; set; }
+        public decimal PesoBruto { get; set; }
+        public decimal PesoLiquido { get; set; }
+        public string NumeroVolume { get; set; }
+        public string NumeroOrigem { get; set; }
+        public string Situacao { get; set; }
+        public DateTime DataEntrada { get; set; }
+        public string Nf { get; set; }
+        public string Serie { get; set; }
+        public string seqNf { get; set; }
+        public string Fornecedor { get; set; }
+        public string NomeFornecedor { get; set; }
+        public string Obs { get; set; }
+
+
+
+    }
+}
