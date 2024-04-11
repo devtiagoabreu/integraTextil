c9ef236df057637ec688b5bd5a1d143577480f7c
Tiago Abreu <devtiagoabreu@gmail.com>
2024-04-11 12:18:38 -0300

    Criando Classe DAO (Data Access Object)  para acessar o dados exportado dos teares

--- DAO/DAODadosTeares.cs
+++ DAO/DAODadosTeares.cs
@@ -4,9 +4,80 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace integraTextil.DAO
+namespace DAO
 {
-    internal class DAODadosTeares
+    public class DAODadosTeares
     {
+        public string? DataTurno { get; set; }
+        public string? Tear { get; set; }
+        public string? Artigo { get; set; }
+        public string? None { get; set; }
+        public string? ArtigoGen { get; set; }
+        public string? Rpm { get; set; }
+        public string? Eficiencia { get; set; }
+        public string? Funcionando { get; set; }
+        public string? Parado { get; set; }
+        public string? Pontos { get; set; }
+        public string? Metros { get; set; }
+        public string? Jardas { get; set; }
+        public string? MedidaGen { get; set; }
+        public string? QtdGen { get; set; }
+        public string? MinGen { get; set; }
+        public string? QtdParadasUrdume { get; set; }
+        public string? MinParadasUrdume { get; set; }
+        public string? QtdParadasOurelaFalsa { get; set; }
+        public string? MinParadasOurelaFalsa { get; set; }
+        public string? QtdParadasLenoDireita { get; set; }
+        public string? MinParadasLenoDireita { get; set; }
+        public string? QtdParadasLenoEsquerda { get; set; }
+        public string? MinParadasLenoEsquerda { get; set; }
+        public string? QtdParadasTrama { get; set; }
+        public string? MinParadasTrama { get; set; }
+        public string? QtdTrocaDeRolo { get; set; }
+        public string? MinTrocaDeRolo { get; set; }
+        public string? QtdCorteTecido { get; set; }
+        public string? MinCorteTecido { get; set; }
+        public string? QtdParadaManual { get; set; }
+        public string? MinParadaManual { get; set; }
+        public string? QtdEnergiaDesligada { get; set; }
+        public string? MinEnergiaDesligada { get; set; }
+        public string? QtdParadasOutras { get; set; }
+        public string? MinParadasOutras { get; set; }
+        public string? Wf11 { get; set; }
+        public string? Wf12 { get; set; }
+        public string? Wf21 { get; set; }
+        public string? Wf22 { get; set; }
+        public string? QtdGen1 { get; set; }
+        public string? MinGen1 { get; set; }
+        public string? QtdGen2 { get; set; }
+        public string? MinGen2 { get; set; }
+        public string? QtdGen3 { get; set; }
+        public string? MinGen3 { get; set; }
+        public string? QtdGen4 { get; set; }
+        public string? MinGen4 { get; set; }
+        public string? QtdGen5 { get; set; }
+        public string? MinGen5 { get; set; }
+        public string? QtdGen6 { get; set; }
+        public string? MinGen6 { get; set; }
+        public string? QtdGen7 { get; set; }
+        public string? MinGen7 { get; set; }
+        public string? QtdGen8 { get; set; }
+        public string? MinGen8 { get; set; }
+        public string? QtdGen9 { get; set; }
+        public string? MinGen9 { get; set; }
+        public string? QtdGen10 { get; set; }
+        public string? MinGen10 { get; set; }
+        public string? QtdGen11 { get; set; }
+        public string? MinGen11 { get; set; }
+        public string? QtdGen12 { get; set; }
+        public string? MinGen12 { get; set; }
+        public string? QtdGen13 { get; set; }
+        public string? MinGen13 { get; set; }
+        public string? QtdGen14 { get; set; }
+        public string? MinGen14 { get; set; }
+        public string? QtdGen15 { get; set; }
+        public string? MinGen15 { get; set; }
+        public string? QtdGen16 { get; set; }
+        public string? MinGen16 { get; set; }
     }
 }
